41830f04de9d22d27db43427a32b4e1075c3e052
Brandon Drake <brandon.drake@teem.com>
2017-12-18 09:37:06 -0700

    Added room joining

--- app/application/route.js
+++ app/application/route.js
@@ -2,11 +2,19 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { get } from '@ember/object';
 
+import { later } from '@ember/runloop';
+
 export default Route.extend({
   socket: service('socket-io'),
 
   activate() {
-    const socket = get(this, 'socket').socketFor('http://localhost:5001/');
+    const channel = parseInt((Math.random() * 2 + 1).toFixed(0));
+
+    console.log('Connected to ', channel);
+
+    const socket = get(this, 'socket').socketFor('http://localhost:5001/manage');
+
+    socket.emit('join', `company_${channel}`);
 
     socket.on('rt-change', function(message) {
       console.log(message);
@@ -14,7 +22,7 @@ export default Route.extend({
   },
 
   deactivate() {
-    const socket = get(this, 'socket').socketFor('http://localhost:5001/');
+    const socket = get(this, 'socket').socketFor('http://localhost:5001/manage');
 
     socket.off('rt-change');
   },
